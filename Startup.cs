05dfb99ea740a5dd1e35bf4bbe4771f98d775672
jgardner-hub <90541201+jgardner-hub@users.noreply.github.com>
2022-04-07 13:31:43 -0600

    Changed connection strings in appsettings.json and startup

--- Startup.cs
+++ Startup.cs
@@ -32,8 +32,11 @@ namespace INTEX
 
 
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            {
+                options.UseMySql(Configuration["ConnectionStrings:DefaultConnection"]);
+
+            });
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
